e9b3a5d990bcaf91960b3d5a6999d312c7ab38e9
mehakhina18 <mehakhina18@gmail.com>
2026-06-17 06:58:30 -0700

    Update product.js

--- product.js
+++ product.js
@@ -1,1 +1,27 @@
+<script>
+const WHATSAPP = "923152950176";
+
+const products = [
+  { 
+    id: 1,
+    name: "Chocolate Bouquet",
+    price: 12000,
+    desc: "Sweetness Wrapped In Love",
+    fullDesc: "A premium handcrafted chocolate bouquet designed for gifting on birthdays, anniversaries, and special occasions.",
+    img: "product1.jpg",
+    category: ["Women", "Men"],
+    included: "Nutella, Kinder Joy, Lots Of Chocolates"
+  },
+  { 
+    id: 2,
+    name: "Makeup Bouquet",
+    price: 8000,
+    desc: "Perfect Gift For Beauty Lover",
+    fullDesc: "A beautifully arranged makeup bouquet curated for beauty enthusiasts who love elegant and thoughtful gifts.",
+    img: "product2.jpg",
+    category: ["Women"],
+    included: "Makeup items arranged in bouquet style"
+   },
+ 
+</script>
 
